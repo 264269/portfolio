71ed612702108279ee524362ac44658b5ab92166
DBECTN WECTON <999all@mail.ru>
2020-12-18 04:42:08 +0700

    Lab 6 in progress! :D

---
+++ port/popup.js
@@ -0,0 +1,6 @@
+document.querySelector('.show.window').onclick = function() {
+    document.querySelector('dialog.window').showModal();
+};
+document.querySelector('.close.window').onclick = function() {
+    document.querySelector('dialog.window').close();
+};
